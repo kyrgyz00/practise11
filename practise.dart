806acc1ed9189d93b4fdcc25d6841c118923e32e
kyrgyz00 <bekbolottashpolotov@gmail.com>
2022-10-31 19:03:03 +0600

    changed

--- practise.dart
+++ practise.dart
@@ -1,13 +1,11 @@
 import 'dart:math';
 
-
 void main() {
   // task_0();
   // task_1();
   // task_2();
   // task_3(4);
   task_4();
- 
 }
 
 task_0() {
@@ -36,32 +34,34 @@ task_2() {
 
 task_3(int x) {
   List<int> arr = [];
-
+  List array = [];
   for (int i = 0; i <= Random().nextInt(100); i++) {
     arr.add(Random().nextInt(5));
   }
+
+  for (int i = 0; i < arr.length; i++) {
+    if (arr[i] == x) {
+      array.add(i);
+    }
+  }
   print(arr);
-  print((arr).indexOf(x));
+  print(array);
 }
 
 task_4() {
   List<int> arr = [];
 
   for (int i = 0; i <= Random().nextInt(100); i++) {
-    arr.add(Random().nextInt(100));
+    arr.add(50+Random().nextInt(101-50));
   }
   // ;
   print(arr);
-  
+
   int i = 1;
-  while (i < arr.length -1) {
-    if (arr[i] == arr[i +1] ) {
+  while (i < arr.length - 1) {
+    if (arr[i] == arr[i + 1]) {
       print("${arr[i]} has equal number");
     } 
-    else {
-      print("no equal");
-    }
-    
     i++;
   }
 }
